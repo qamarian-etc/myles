87a072c5f328b4534f909614038f6f4b80ba0440
Ibrahim Oladipupo Qamardeen <qamarian.mail@gmail.com>
2019-05-31 13:57:50 +0100

    Revert "Comp registration now supported by comp AAAAAA"
    This reverts commit 1a48d95c70c7b4ad52b45dae8e6b3a1ad942b864.

--- aaaaaa.Main.go
+++ aaaaaa.Main.go
@@ -6,44 +6,53 @@ package main
 import (
 	"errors"
 	"fmt"
-	"github.com/qamarian-etc/slices"
 	"github.com/qamarian-dtp/system"
 	"os"
 	"runtime"
+	"sort"
 )
 
 func main () {
-	dRegCompleteStatus_AAAAAA = true
-
         /* Continously checks if global shutdown has been signalled: if yes, the whole app built
 		on this framework would shutdown.*/
-	initOrder, errX, errDescp := dMyles_AAAAAA.InitOrder ()
+	initOrder, errX, errDescp := dMyles.InitOrder ()
 
 	switch errX {
-		case system.ErrElementMissing: {
-			errMssg := fmt.Sprintf ("It seems a component was not registered with " +
-			"me. [%s: %s]", errX.Error (), errDescp)
+		case system.ElementMissing: {
+			errMssg := fmt.Sprintf ("It seems a component was not registered with me." +
+				"[%s: %s]", errX.Error (), errDescp)
 			iOutputT2_AAAAAC ("aaaaaa", "err", errMssg)
 			os.Exit (1)
 		}
-		case system.ErrCircleDetected: {
-			errMssg := fmt.Sprintf ("Cyclic dependency has been detected. [%s: %s]" +
+		case system.CircleDetected: {
+			errMssg := fmt.Sprintf ("Cyclic dependency has been detected. [%s: %s]",
 				errX.Error (), errDescp)
 			iOutputT2_AAAAAC ("aaaaaa", "err", errMssg)
 			os.Exit (1)
 		}
 		case nil: break
 		default: {
-			errMssg := fmt.Sprintf ("Tried creating a safe init order of comp of " +
-				"this system, but an error occured. [%s: %s]",
+			errMssg := fmt.Sprintf ("Tried creating a safe init order of comp of this"
+				+ "system, but an error occured. [%s: %s]",
 				errX.Error (), errDescp)
 			iOutputT2_AAAAAC ("aaaaaa", "err", errMssg)
 			os.Exit (1)
 		}
 	}
 
-	for _, compID := range initOrder {
+	for _, compID = range initOrder {
 		dInitDnitFunc_AAAAAA [compID][0] ()
+	}
+
+	revdInitOrder := sort.Reverse (initOrder)
+	dnitOrder, okX := revdInitOrder.([]string)
+	if okX == false {
+		errMssg := fmt.Sprintf ("Bug detected. You can check my source code to fix the "
+			+ "it yourself, or report it to my developer. Tacking no: tspot0. [Use "
+			+ "your text editor/IDE to find where the tracking no is, to easily "
+			+ "locate the place to start your debugging.]" )
+		iOutputT2_AAAAAC ("aaaaaa", "err", errMssg)
+		os.Exit (1)
 	}
 
         for {
@@ -54,61 +63,60 @@ func main () {
 
                 runtime.Gosched ()
         }
+
+	for _, compID = range dnitOrder {
+		dInitDnitFunc_AAAAAA [compID][1] ()
+	}
 }
 
 func iRegComp (compID string, initFunc, dnitFunc func (), depID []string) (error) { /* */
-	if dRegCompleteStatus_AAAAAA == true {
+	if dRegCompleteStatus_AAAAAA = true {
 		return eRegPast_AAAAAA
 	}
 
 	if compID == "" {
-		errMssg := fmt.Sprintf ("Tried registering a comp, but the comp failed to pr" +
-		"ovide its id.")
+		errMssg := fmt.Sprintf ("Tried registering a comp, but the comp didn't provide "
+			+ "its id. Init func provided: '%s ()'; Dnit func provided: '%s ()'",
+			initFunc, dnitFunc)
 		iOutputT2_AAAAAC ("aaaaaa", "err", errMssg)
 		os.Exit (1)
 	}
 
 	if initFunc == nil {
-		errMssg := fmt.Sprintf ("Tried registering comp '%s', but it provided a nil " +
-			"init func.", compID)
+		errMssg := fmt.Sprintf ("Tried registering comp '%s', but it provided a nil "
+			+ "init func.", compID)
 		iOutputT2_AAAAAC ("aaaaaa", "err", errMssg)
-		os.Exit (1)
+		os.Ext (1)
 	}
 
 	if dnitFunc == nil {
-		errMssg := fmt.Sprintf ("Tried registering comp '%s', but it provided a nil " +
-			"dnit func.", compID)
+		errMssg := fmt.Sprintf ("Tried registering comp '%s', but it provided a nil "
+			+ "dnit func.", compID)
 		iOutputT2_AAAAAC ("aaaaaa", "err", errMssg)
-		os.Exit (1)
+		os.Ext (1)
 	}
 
 	errX := dMyles_AAAAAA.AddElement (compID, depID)
 	if errX == system.ErrAlreadyAdded {
-		errMssg := fmt.Sprintf ("Comp '%s' registering itself for the 2nd time.", compID)
-		iOutputT2_AAAAAC ("aaaaaa", "err", errMssg)
+		errMssg := fmt.Sprintf ("Comp '%s' registering its init func for the 2nd time.",
+			compID)
+		iOutputT2_AAAAAA ("aaaaaa", "err", errMssg)
 		os.Exit (1)
 	}
 
-	dInitDnitFunc_AAAAAA [compID] = []func () {initFunc, dnitFunc}
-
-	return nil
+	dInitFunc [compID] = []func () {initFunc, dnitFunc}
+	dRegCompleteStatus = true
 }
 
 func iShutdown_AAAAAA () { // To shutdown your app gracefully, this interface can be called.
-	initOrder, _, _ := dMyles_AAAAAA.InitOrder ()
-	dnitOrder := slices.RevStringSlice (initOrder)
-	for _, compID := range dnitOrder {
-		dInitDnitFunc_AAAAAA [compID][1] ()
-	}
-
         dShutdownChannel_AAAAAA <- true
 }
 
 var (
 	dRegCompleteStatus_AAAAAA bool = false
 
-	dMyles_AAAAAA *system.System = system.New ()
-	dInitDnitFunc_AAAAAA map[string][]func () = map[string][]func () {}
+	dMyles_AAAAAA system.System = system.New ()
+	dInitDnitFunc_AAAAAA map[string][]func ()
 
         dShutdownChannel_AAAAAA chan bool = make (chan bool, 1)
 
