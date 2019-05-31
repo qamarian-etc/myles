65fd2062f1d6db8788e89c41d46e74cbc3722784
Ibrahim Oladipupo Qamardeen <qamarian.mail@gmail.com>
2019-05-31 20:45:57 +0100

    Comp aaaaai ready for version 3

--- aaaaai.CriticalEventZain.go
+++ aaaaai.CriticalEventZain.go
@@ -1,6 +1,15 @@
 package main
 
-/* Component is yet to be developed. Its file exist at the moment, only to reserve the ID "aaaaai", for this component. */
+/* Component is yet to be developed. Its file exist at the moment, only to reserve the ID
+	"aaaaai", for this component. */
+
+func init () {
+	iRegComp ("aaaaai", iInit_AAAAAI, iDnit_AAAAAI, nil)
+}
+
+func iInit_AAAAAI () {}
+
+func iDnit_AAAAAI () {}
 
 func iBeInformed_AAAAAI (eventDescription string) { /*
 
