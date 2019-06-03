ba04a27d30cf91141a25899c3f2665bce8ae25e7
Ibrahim Oladipupo Qamardeen <qamarian.mail@gmail.com>
2019-06-03 23:25:30 +0100

    Comp AAAAAI about having portable interface

---
+++ aaaaai.zI.go
@@ -0,0 +1,18 @@
+package main
+
+/* Component is yet to be developed. Its file exist at the moment, only to reserve the ID
+	"aaaaai", for this component. */
+
+func init () {
+	iRegComp_AAAAAA ("aaaaai", iInit_AAAAAI, iDnit_AAAAAI, nil)
+}
+
+func iInit_AAAAAI () {}
+
+func iDnit_AAAAAI () {}
+
+func iBeInformed_AAAAAI (eventDescription string) { /*
+
+	INPUT
+	input 0: Description of the critical event that occured. */
+}
